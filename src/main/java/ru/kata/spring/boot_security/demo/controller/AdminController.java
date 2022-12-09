8501fda53287350029861e9e430971f92cd497ba
Lomenzo <LomenzoJVM@gmail.com>
2022-12-09 15:25:12 +0800

    v.9.1 add AdminController

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -0,0 +1,72 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.services.UserService;
+
+import java.util.List;
+
+@Controller
+public class AdminController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping(value = "/admin/userApp")
+    public String showUserForm(Model model, ModelMap modelMap, Model modelRole) {
+        model.addAttribute("userFormer", new User());
+        List<User> userList = userService.getUserList();
+        modelMap.addAttribute("users", userList);
+        modelRole.addAttribute("roleFormer", new Role());
+        return "adminPage";
+    }
+
+    @PostMapping(value = "/admin/userApp")
+    public String addUserPostMethod(Model model, @ModelAttribute("userFormer") User userForm, Model modelRole, ModelMap modelMap) {
+        model.addAttribute("userFormer", userForm);
+        //modelRole.addAttribute("roleFormer", roleForm);
+
+
+//        List<String> options = new ArrayList<String>();
+//        options.add("option 1");
+//        options.add("option 2");
+//        modelRole.addAttribute("options", options);
+//        System.out.println(modelRole.toString().concat(" - roleForm give THAT"));
+
+        String name = userForm.getName();
+        String password = userForm.getPassword();
+        int salary = userForm.getSalary();
+        User newUser = new User(name,password,salary);
+        userService.saveUser(newUser);
+        List<User> userList = userService.getUserList();
+        modelMap.addAttribute("users", userList);
+        return "adminPage";
+    }
+
+    @GetMapping("/admin/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        model.addAttribute("userFinded", new User());
+        return "updateUserPage";
+    }
+
+    @PostMapping("/admin/update/{id}")
+    public String updateUser(@PathVariable("id") long id, @ModelAttribute("userFinded") User userFinded, Model model) {
+        model.addAttribute("userFinded", userFinded);
+        userService.updateUser(userFinded, id);
+        return "updateUserPage";
+    }
+
+    @GetMapping("/admin/delete/{id}")
+    public String deleteUser(@PathVariable("id") long id, Model model) {
+        userService.deleteUserById(id);
+        return "deletedUserPage";
+    }
+}
--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -17,7 +17,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@EnableAutoConfiguration
 public class UserController {
 
     @Autowired
@@ -32,61 +31,4 @@ public class UserController {
         modelAndView.addObject("user", user);
         return modelAndView;
     }
-
-    @GetMapping(value = "/admin/userApp")
-    public String showUserForm(Model model, ModelMap modelMap, Model modelRole) {
-        model.addAttribute("userFormer", new User());
-        List<User> userList = userService.getUserList();
-        modelMap.addAttribute("users", userList);
-        modelRole.addAttribute("roleFormer", new Role());
-        return "adminPage";
-    }
-
-    @PostMapping(value = "/admin/userApp")
-    public String addUserPostMethod(Model model, @ModelAttribute("userFormer") User userForm, Model modelRole, ModelMap modelMap) {
-        model.addAttribute("userFormer", userForm);
-        //modelRole.addAttribute("roleFormer", roleForm);
-
-
-//        List<String> options = new ArrayList<String>();
-//        options.add("option 1");
-//        options.add("option 2");
-//        modelRole.addAttribute("options", options);
-//        System.out.println(modelRole.toString().concat(" - roleForm give THAT"));
-
-        String name = userForm.getName();
-        String password = userForm.getPassword();
-        int salary = userForm.getSalary();
-        User newUser = new User(name,password,salary);
-        userService.saveUser(newUser);
-        List<User> userList = userService.getUserList();
-        modelMap.addAttribute("users", userList);
-        return "adminPage";
-    }
-
-    @GetMapping("/admin/edit/{id}")
-    public String showUpdateForm(@PathVariable("id") long id, Model model) {
-        model.addAttribute("userFinded", new User());
-        return "updateUserPage";
-    }
-
-    @PostMapping("/admin/update/{id}")
-    public String updateUser(@PathVariable("id") long id, @ModelAttribute("userFinded") User userFinded, Model model) {
-        model.addAttribute("userFinded", userFinded);
-        userService.updateUser(userFinded, id);
-        return "updateUserPage";
-    }
-
-    @GetMapping("/admin/delete/{id}")
-    public String deleteUser(@PathVariable("id") long id, Model model) {
-        userService.deleteUserById(id);
-        return "deletedUserPage";
-    }
-
-    @DeleteMapping("/{id}")
-    public String findById(@PathVariable long id) {
-        userService.deleteUserById(id);
-        return "redirect:/user";
-    }
-
 }
